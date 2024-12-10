708a60f4375951ddef09074efe1cd7772c967226
nathanfekade <nathanfekade276@gmail.com>
2024-12-10 14:33:14 +0300

    updated print values

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,6 +5,6 @@ sum = number1+number2
 difference = number1-number2
 product = number1*number2
 
-print("sum of ", number1, "and", number2 , "is", sum)
-print("difference of ", number1, "and", number2 , "is", difference)
-print("product of ", number1, "and", number2 , "is", product)
+print("Addition of", number1, "and", number2 , "is", sum)
+print("Subtraction of", number1, "and", number2 , "is", difference)
+print("Multiplication of", number1, "and", number2 , "is", product)
