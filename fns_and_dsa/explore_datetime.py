455c9cce7ac05e83c48f0db2fdb7828c5f8a8629
nathanfekade <nathanfekade276@gmail.com>
2025-01-17 00:47:30 +0300

    updated explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,6 +3,7 @@ from datetime import timedelta, date, datetime
 def display_current_datetime():
     current_date = datetime.now()
     print('Current date and time:',current_date)
+    return current_date
 
 
 def calculate_future_date():
