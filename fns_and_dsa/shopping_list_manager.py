c5265ee827b483df72ed6f4fa6b414bf166ced48
nathanfekade <nathanfekade276@gmail.com>
2025-01-16 23:08:08 +0300

    updated shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,29 +15,29 @@ def main():
         try:
             choice = input("Enter your choice: ")
             int(choice)
-            if choice == '1':
-                item_name = input("Item name: ")
-                shopping_list.append(item_name)
-
-            elif choice == '2':
-                item_name = input("Item name: ")
-                if item_name in shopping_list:
-                    shopping_list.remove(item_name)
-                else:
-                    print("item does not exist")
-
-            elif choice == '3':
-                for item in shopping_list:
-                    print(item)
-            elif choice == '4':
-                print("Goodbye!")
-                break
-            else:
-                print("Invalid choice. Please try again.")
-
+            
         except ValueError:
                 print("please enter a valid number")
         
-        
+        if choice == '1':
+                item_name = input("Item name: ")
+                shopping_list.append(item_name)
+
+        elif choice == '2':
+            item_name = input("Item name: ")
+            if item_name in shopping_list:
+                shopping_list.remove(item_name)
+            else:
+                print("item does not exist")
+
+        elif choice == '3':
+            for item in shopping_list:
+                print(item)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
 if __name__ == "__main__":
     main()
