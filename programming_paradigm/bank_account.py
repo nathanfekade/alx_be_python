209821271be686c239442157d6b8c6bb65c7931a
nathanfekade <nathanfekade276@gmail.com>
2025-01-22 08:53:50 +0300

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,4 +14,4 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Your current balance is: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
