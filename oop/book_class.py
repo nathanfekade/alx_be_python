7427727c67ff93b7c1b885e2e33ba459fb89fad8
nathanfekade <nathanfekade276@gmail.com>
2025-02-02 20:26:09 +0300

    updated with docustrings book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,15 +1,18 @@
 class Book:
-    
-    def __init__(self, title, author, year) :
+    def __init__(self, title, author, year):
+        """Initializes a Book instance with title, author, and year."""
         self.title = title
         self.author = author
         self.year = year
-    
-    def __del__(self) :
+
+    def __del__(self):
+        """Prints "Deleting (title of the book)" upon object deletion."""
         print(f"Deleting {self.title}")
-    
-    def __str__(self) :
+
+    def __str__(self):
+        """Returns a string in the format "(title) by (author), published in (year)"."""
         return f"{self.title} by {self.author}, published in {self.year}"
-    
-    def __repr__(self) :
-        return f"Book ('{self.title}', '{self.author}', {self.year})"
+
+    def __repr__(self):
+        """Returns a string that would recreate the Book instance: f"Book('{self.title}', '{self.author}', {self.year})"."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
